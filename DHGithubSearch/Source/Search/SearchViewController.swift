d4ad35468aec5b048efb98ee4b72a0f963a8f6a9
David Freitas <davidfreitass@gmail.com>
2022-05-08 18:47:50 +0200

    Create XCode project

---
+++ DHGithubSearch/Source/Search/SearchViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SearchViewController.swift
+//  DHGithubSearch
+//
+//  Created by David Freitas on 08/05/2022.
+//
+
+import Foundation
+import UIKit
+
+final class SearchViewController: UIViewController {
+}
